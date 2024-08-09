ad97302d18c268361e3bda6efd0807d1247d68b5
Dmytro Yaremyshyn <dmytroy@dxspark.com>
2024-08-09 19:37:11 +0100

    Base project created

---
+++ Population/Population/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Population
+//
+//  Created by Dmytro Yaremyshyn on 09/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
